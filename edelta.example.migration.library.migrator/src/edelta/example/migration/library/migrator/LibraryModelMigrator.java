5f8e42884b0302c72a5e26c97aff66992e37b538
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-05-28 16:09:08 +0200

    implemented migrator

---
+++ edelta.example.migration.library.migrator/src/edelta/example/migration/library/migrator/LibraryModelMigrator.java
@@ -0,0 +1,26 @@
+package edelta.example.migration.library.migrator;
+
+import java.util.Collection;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+import books.BooksPackage;
+import edelta.FromV1ToV2;
+import edelta.lib.EdeltaVersionMigrator;
+import library.LibraryPackage;
+
+public class LibraryModelMigrator {
+
+	public Collection<Resource> execute(String modelPath) throws Exception {
+		var edeltaMigrator = new EdeltaVersionMigrator();
+		// load the current (latest) version of EPackages
+		edeltaMigrator.loadCurrentEPackages(LibraryPackage.eINSTANCE, BooksPackage.eINSTANCE);
+		// register the Edelta migration code
+		edeltaMigrator.registerMigration(FromV1ToV2::new);
+		// load the models to check and migrate
+		edeltaMigrator.addModelFileExtensions(".library", ".books");
+		edeltaMigrator.loadModelsFrom(modelPath);
+		// let the Edelta framework do the rest
+		return edeltaMigrator.execute();
+	}
+}
