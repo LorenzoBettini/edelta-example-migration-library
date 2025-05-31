0ebbeba2d1e2754fc0291953715d183fe6ecbb55
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-05-31 13:59:50 +0200

    model migration with copyGroupingCountingRule

--- edelta.example.migration.library.migrator/edelta-gen/edelta/FromV3ToV4.java
+++ edelta.example.migration.library.migrator/edelta-gen/edelta/FromV3ToV4.java
@@ -1,25 +1,15 @@
 package edelta;
 
 import edelta.lib.EdeltaDefaultRuntime;
-import edelta.lib.EdeltaEcoreUtil;
 import edelta.lib.EdeltaEngine;
 import edelta.lib.EdeltaModelMigrator;
 import edelta.lib.EdeltaRuntime;
 import edelta.lib.annotation.EdeltaGenerated;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import org.eclipse.emf.ecore.EAttribute;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.xtext.xbase.lib.IterableExtensions;
 
 @SuppressWarnings("all")
 public class FromV3ToV4 extends EdeltaDefaultRuntime {
@@ -36,33 +26,7 @@ public class FromV3ToV4 extends EdeltaDefaultRuntime {
     final EReference bookFeature = getEReference("library", "BookItem", "book");
     final EAttribute numOfCopiesFeature = getEAttribute("library", "BookItem", "numOfCopies");
     final Consumer<EdeltaModelMigrator> _function_1 = (EdeltaModelMigrator it_1) -> {
-      final EdeltaModelMigrator.CopyProcedure _function_2 = (EStructuralFeature oldBookItemsFeature, EObject oldLibrary, EObject newLibrary) -> {
-        final List<EObject> origElements = EdeltaEcoreUtil.getValueAsList(oldLibrary, oldBookItemsFeature);
-        final Function<EObject, EObject> _function_3 = (EObject oldBookItem) -> {
-          return EdeltaEcoreUtil.getValueAsEObject(oldBookItem, it_1.<EReference>getOriginal(bookFeature));
-        };
-        final Supplier<LinkedHashMap<EObject, List<EObject>>> _function_4 = () -> {
-          return new LinkedHashMap<EObject, List<EObject>>();
-        };
-        final LinkedHashMap<EObject, List<EObject>> groupedByBook = origElements.stream().collect(
-          Collectors.groupingBy(_function_3, _function_4, 
-            Collectors.<EObject>toList()));
-        final Function<Map.Entry<EObject, List<EObject>>, EObject> _function_5 = (Map.Entry<EObject, List<EObject>> entry) -> {
-          EObject _xblockexpression = null;
-          {
-            EObject oldBookItem = IterableExtensions.<EObject>head(entry.getValue());
-            final Consumer<EObject> _function_6 = (EObject it_2) -> {
-              it_2.eSet(numOfCopiesFeature, Integer.valueOf(entry.getValue().size()));
-            };
-            _xblockexpression = it_1.createFrom(it_1.<EClass>getMigrated(oldBookItem.eClass()), oldBookItem, _function_6);
-          }
-          return _xblockexpression;
-        };
-        final List<EObject> newBookItems = groupedByBook.entrySet().stream().<EObject>map(_function_5).toList();
-        newLibrary.eSet(bookItemsFeature, newBookItems);
-      };
-      it_1.copyRule(
-        it_1.<EStructuralFeature>wasRelatedTo(bookItemsFeature), _function_2);
+      it_1.copyGroupingCountingRule(bookItemsFeature, bookFeature, numOfCopiesFeature);
     };
     this.modelMigration(_function_1);
   }
